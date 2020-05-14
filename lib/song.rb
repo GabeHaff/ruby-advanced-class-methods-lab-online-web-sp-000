b7067c3efa448c8d7c40b7ad7adb5ccb3630f9fb
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-05-14 11:07:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,8 +48,8 @@ def Song.new_from_filename(file_name) ##class constructor
 end
 
 def self.create_from_filename(file_name)
-  song.new_from_filename(file_name)
- song.save
+  Song.new_from_filename(file_name)
+ Song.save
   new_song 
 end 
 end 
