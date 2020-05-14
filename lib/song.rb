33d0390ba12498cbd1c889c5d2316c13c0b346f3
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-05-14 10:19:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,8 +38,11 @@ def self.new_from_file_name(file_name) #class constructor
 new_song.name = file_name.split(" - ")[1].chomp(".mp3")
 new_song.artist_name = file_name.split(" - ")[0]
 new_song
-## class:song, self=new_song. given "taylor swift- Blank space.mp3"
+## class:song, self=new_song. given 
+#"taylor swift- Blank space.mp3", or "file_name", 
+#remove the dashes and the "mp3" from the end 
 end
+  
   
   
  
