8c6625f53e2be7810a486f7b94f1897ea5056027
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-05-14 10:42:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,14 +34,10 @@ def self.alphabetical #also a class method
 end 
   
 def self.new_from_file_name(file_name) #class constructor 
-  new_song = self.new
- new_song.name = file_name.split(" - ")[1].chomp(".mp3")
-new_song.artist_name = file_name.split(" - ")[0]
+  new_song = self.create
+  new_song.name = file_name.split(" - ")[1].chomp(".mp3")
+  new_song.artist_name = file_name.split(" - ")[0]
   new_song
-## class:song, self=new_song. given 
-#"taylor swift- Blank space.mp3", or "file_name", 
-#remove the dashes and the "mp3" from the end , separating into 
-#"name, artist, based on argument file_name"
 end
 
   
